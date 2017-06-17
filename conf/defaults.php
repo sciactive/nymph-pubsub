fc388cd0bec5c6e1d1bf62a603757f3bef475ae7
Hunter Perrin <hperrin@gmail.com>
2017-06-16 18:42:17 -0700

    Updated license to Apache-2.0.

--- conf/defaults.php
+++ conf/defaults.php
@@ -3,7 +3,7 @@
  * Nymph-PubSub's configuration defaults.
  *
  * @package Nymph-PubSub
- * @license http://www.gnu.org/licenses/lgpl.html
+ * @license https://www.apache.org/licenses/LICENSE-2.0
  * @author Hunter Perrin <hperrin@gmail.com>
  * @copyright SciActive.com
  * @link http://sciactive.com/
